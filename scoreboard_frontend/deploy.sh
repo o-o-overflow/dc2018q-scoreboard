96dccd267bdc2b011ffed5871daa3f32e67fcf63
Bryce Boe <bbzbryce@gmail.com>
2018-05-15 22:15:47 -0400

    Fix eslint issues in other frontends

--- scoreboard_frontend/deploy.sh
+++ scoreboard_frontend/deploy.sh
@@ -22,6 +22,4 @@ fi
   && rm build/static/*/*.map \
   && rm build/asset-manifest.json \
   && rm build/service-worker.js \
-  && find build/static -regex '.*\.[cj]ss*' -exec sed -i '' '/^\/[/*]# sourceMappingURL/ d' {} \; \
-  && aws --profile ooo s3 cp ./build/index.html s3://oooverflow-scoreboard/$BUILD/index.html --cache-control max-age=60 \
-  && aws --profile ooo s3 sync build/ s3://oooverflow-scoreboard/$BUILD
+  && find build/static -regex '.*\.[cj]ss*' -exec sed -i '' '/^\/[/*]# sourceMappingURL/ d' {} \;
